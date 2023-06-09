012522100ace40157cf27d21f62c6704629ff6a6
Abdurakhmon Jamoliddinov <jamoliddinovabdu@gmail.com>
2023-06-09 20:26:35 +0500

    Initial Commit

---
+++ Time/TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeApp.swift
+//  Time
+//
+//  Created by Workspace (Abdurakhmon Jamoliddinov) on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
